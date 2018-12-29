adee661e35c6ed6f8e56061c312e450dbbd713eb
SamLingle <41877457+SamLingle@users.noreply.github.com>
2018-12-29 15:50:21 -0600

    Create scrape_mars.py

---
+++ scrape_mars.py
@@ -0,0 +1,8 @@
+# Import dependencies
+from bs4 import BeautifulSoup
+from spinter import Browser
+import pandas as pd
+import requests
+
+# Create a function called scrape to execute scraping code and return one dictionary containingf all scraped data
+def 
